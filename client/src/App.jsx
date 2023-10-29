ac229b0a71a1bc7ffccf4dd91043e947a221f0c9
ADORA-ATAYA <pragyanshutayal2100@gmail.com>
2023-10-29 13:13:39 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
