453049e1446eb769484ecba15d9bfc6a74122759
ADORA-ATAYA <pragyanshutayal2100@gmail.com>
2023-10-29 13:33:29 +0530

    coonect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = express();
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Database connected...")
+}).catch((error)=>{
+    console.log(error)
+})
 
 app.listen(3000,()=>{
     console.log("server running on port 3000 ...");
