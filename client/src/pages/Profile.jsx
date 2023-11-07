2edb2344f49f7955b6ffb4b064e9a761827cf694
ADORA-ATAYA <pragyanshutayal2100@gmail.com>
2023-11-07 22:34:56 +0530

    complete image upload funtionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,68 @@
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from '../firebase';
+
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [File, setFile] = useState(undefined);
+  const [FilePer, setFilePer] = useState(0)
+  const [fileUploadError, setfileUploadError] = useState(false)
+  const [FormData, setFormData] = useState({})
+  useEffect(()=>{
+    if(File){
+      handleFileUpload( File);
+    }
+  },[File]);
+  const handleFileUpload = (file)=>{
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage,fileName);
+    const uploadTask = uploadBytesResumable(storageRef , file);
+
+    uploadTask.on('state_changed' , (snapshot)=>{
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes)*100;
+      setFilePer(Math.round(progress));
+    },
+    (error) =>{
+      setfileUploadError(true);
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref)
+      .then((downloadURL)=>{
+        setFormData({...FormData , avatar : downloadURL});
+      });
+    }
+    );
+  }
+  const fileRef = useRef(null)
   return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
     <form className='flex flex-col gap-4'>
-      <img
-        src={currentUser.avatar}
+      <input onChange={(e)=>setFile(e.target.files[0])}
+      type="file" ref = {fileRef} accept='image/*' hidden/>
+      <img onClick={()=>fileRef.current.click()}
+        src={FormData.avatar || currentUser.avatar}
         alt='profile'
         className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
       />
+      <p className='text-sm self-center'>
+        {fileUploadError ? (
+          <span className='text-red-700'>Error Image Upload 
+          (image must be less then 2mb)</span> 
+          ) :  FilePer>0 && FilePer<100 ? (
+          <span className='text-slate-700'>
+            {`Uploading ${FilePer} %`}
+          </span> )
+          : FilePer ===100 ? (
+          <span className='text-green-700'>
+            Image Successfully Uploaded!
+          </span> )
+          : ("")
+        }
+      </p>
       <input
         type='text'
         placeholder='username'
